08eacbbbc079ed0d67eab837195615c9d9591c90
Karl Heinz Marbaise <info@soebes.de>
2014-03-26 20:16:22 +0100

    - Added first part of impl.

--- src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
+++ src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
@@ -1,11 +1,15 @@
 package com.soebes.maven.plugins.uptodate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.SystemUtils;
 import org.apache.maven.artifact.installer.ArtifactInstaller;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
 import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.execution.ProjectDependencyGraph;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
@@ -45,7 +49,68 @@ public class UpToDateMojo
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
-        mavenSession.getRequest().getSelectedProjects();
+        MavenProject topLevelProject = mavenSession.getTopLevelProject();
+        getLog().info( "TopLevelProject:" + topLevelProject.getId() );
+        List<MavenProject> sortedProjects = mavenSession.getProjects();
+
+        ProjectDependencyGraph projectDependencyGraph = mavenSession.getProjectDependencyGraph();
+        projectDependencyGraph.getSortedProjects();
+
+        //This is for things like mvn -pl a,b,c clean package 
+        List<String> selectedProjects = mavenSession.getRequest().getSelectedProjects();
+        if ( selectedProjects != null && !selectedProjects.isEmpty() )
+        {
+            for ( String selectedProject : selectedProjects )
+            {
+                getLog().info( "Selected projects:" + selectedProject );
+            }
+        }
+        if ( sortedProjects != null && !sortedProjects.isEmpty() )
+        {
+            for ( MavenProject mavenProject : sortedProjects )
+            {
+                getLog().info( "Project:" + mavenProject.getId() );
+            }
+
+            checkReactor( sortedProjects );
+        }
+    }
+
+    private void checkReactor( List<MavenProject> sortedProjects )
+        throws MojoExecutionException
+    {
+        List<MavenProject> consistenceCheckResult = isReactorVersionConsistent( sortedProjects );
+        if ( !consistenceCheckResult.isEmpty() )
+        {
+            StringBuilder sb = new StringBuilder().append( SystemUtils.LINE_SEPARATOR );
+            for ( MavenProject mavenProject : consistenceCheckResult )
+            {
+                sb.append( " --> " );
+                sb.append( mavenProject.getId() );
+                sb.append( SystemUtils.LINE_SEPARATOR );
+            }
+            throw new MojoExecutionException( "The reactor contains different versions." + sb.toString() );
+        }
+    }
+
+    private List<MavenProject> isReactorVersionConsistent( List<MavenProject> projectList )
+    {
+        List<MavenProject> result = new ArrayList<MavenProject>();
+
+        if ( projectList != null && !projectList.isEmpty() )
+        {
+            String version = projectList.get( 0 ).getVersion();
+            getLog().debug( "First version:" + version );
+            for ( MavenProject mavenProject : projectList )
+            {
+                getLog().debug( " -> checking " + mavenProject.getId() );
+                if ( !version.equals( mavenProject.getVersion() ) )
+                {
+                    result.add( mavenProject );
+                }
+            }
+        }
+        return result;
     }
 
 }
