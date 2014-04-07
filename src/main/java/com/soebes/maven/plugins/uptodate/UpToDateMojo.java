c48044e0c76093c48498b115b1d354799a338d85
Karl Heinz Marbaise <info@soebes.de>
2014-04-07 09:00:53 +0200

    - compileable version with Eclipse Aether.

--- src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
+++ src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
@@ -1,18 +1,7 @@
 package com.soebes.maven.plugins.uptodate;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.lang.SystemUtils;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.DefaultRepositoryRequest;
-import org.apache.maven.artifact.repository.RepositoryRequest;
-import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
-import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
-import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -21,7 +10,15 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.sonatype.aether.impl.ArtifactResolver;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.VersionRangeRequest;
+import org.eclipse.aether.resolution.VersionRangeResolutionException;
+import org.eclipse.aether.resolution.VersionRangeResult;
+import org.eclipse.aether.version.Version;
 
 /**
  * @author Karl-Heinz Marbaise <a href="mailto:khmarbaise@soebes.de">khmarbaise@soebes.de</a>
@@ -44,192 +41,48 @@ public class UpToDateMojo
     private MavenSession mavenSession;
 
     @Component
-    protected ArtifactRepositoryFactory artifactRepositoryFactory;
-
-    @Component( role = ArtifactRepositoryLayout.class )
-    private Map<String, ArtifactRepositoryLayout> repositoryLayouts;
+    private RepositorySystem repoSystem;
 
     @Component
-    private ArtifactResolver resolver;
+    private RepositorySystemSession repositorySystemSession;
+    
 
-    @Component
-    private ArtifactInstaller installer;
-
-    /**
-     * The component used to create artifacts.
-     */
-    @Component
-    private ArtifactFactory artifactFactory;
-
-    @Parameter( defaultValue = "${project.remoteArtifactRepositories}", readonly = true )
-    private List<ArtifactRepository> remoteArtifactRepositories;
-
-    /**
-     * The component used to create artifacts.
-     */
-    @Component
-    private ArtifactRepositoryFactory repositoryFactory;
-
-    /**
-     */
-    @Parameter( property = "localRepository", required = true, readonly = true )
-    private ArtifactRepository localRepository;
-
-    public void resultArtifact( String groupId, String artifactId, String version, String type )
-    {
-        Artifact artifact = null;
-        artifact = artifactFactory.createArtifact( groupId, artifactId, version, Artifact.SCOPE_COMPILE, type );
-
-      RepositoryRequest repositoryRequest = DefaultRepositoryRequest.getRepositoryRequest( mavenSession, mavenProject ); 
-      ArtifactResolutionRequest request = new ArtifactResolutionRequest(repositoryRequest);
-      request.setArtifact( artifact );
-
-      ArtifactResolutionResult arr = resolver.resolveArtifact( mavenSession.getRepositorySession(), request )( request );
-
-      ArtifactResolutionResult arr = resolver.resolve( request );
-      arr.isSuccess();
-        
-//        List<String> findVersions = localRepository.findVersions( artifact );
-//        if ( findVersions.isEmpty() )
-//        {
-//            getLog().info( " Didn't found versions" );
-//        }
-//        else
-//        {
-//            for ( String foundVersion : findVersions )
-//            {
-//                getLog().info( "Found version:" + foundVersion );
-//            }
-//
-//        }
-
-//            RepositoryRequest repositoryRequest = 
-//            ArtifactResolutionRequest request = new ArtifactResolutionRequest(repositoryRequest);
-//            request.setArtifact( artifact );
-//
-//            resolver.resolve( request );
-//            ArtifactResolutionResult arr =
-//                    resolver.resolveTransitively(Collections.singleton(artifact), originatingArtifact,
-//                            remoteRepositories, localRepository, artifactMetadataSource);
-//
-//            if (!groupId.equals(artifact.getGroupId()) || !artifactId.equals(artifact.getArtifactId())
-//                    || !version.equals(artifact.getVersion())) {
-//                artifact =
-//                        artifactFactory.createArtifactWithClassifier(groupId, artifactId, version, type, classifier);
-//                copyPoms(artifact, testRepository);
-//            }
-//
-//            for (Artifact arrArtifact : (Set<Artifact>) arr.getArtifacts()) {
-//                copyArtifact(arrArtifact, testRepository);
-//            }
-
-    }
-
+    @Parameter(defaultValue="${project.remoteProjectRepositories}", readonly=true)
+    private List<RemoteRepository> remoteRepositories;
+    
+    
+    @Parameter(defaultValue="${project.remotePluginRepositories}", readonly=true)
+    private List<RemoteRepository> remotePluginRepositories;
+    
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
 
-        resultArtifact( "com.soebes.smpp", "smpp", "0.6.3", "pom" );
-//        ArtifactHandler artifacthandler = new DefaultArtifactHandler();
-//        VersionRange vr = null; //FIXME
-//        try
-//        {
-//            vr = VersionRange.createFromVersionSpec( "[0.6,)" );
-//        }
-//        catch ( InvalidVersionSpecificationException e )
-//        {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        Artifact artifact =
-//            new DefaultArtifact( "com.soebes.smpp", "smpp", vr, "compile", "jar", null, artifacthandler );
-//        ArtifactResolutionRequest request = new ArtifactResolutionRequest();
-//        request.setArtifact( artifact );
-//        ArtifactResolutionResult resolve = resolver.resolve( request );
-//        getLog().info( " Success: " + resolve.isSuccess() );
-//        for ( Artifact item : resolve.getArtifacts() )
-//        {
-//            getLog().info( "Artifact:" + item.getId() );
-//        }
-//        MavenProject topLevelProject = mavenSession.getTopLevelProject();
-//        getLog().info( "TopLevelProject:" + topLevelProject.getId() );
+        Artifact artifact = new DefaultArtifact( "com.soebes.smpp:smpp:[0,)" );
 
-//        ProjectDependencyGraph projectDependencyGraph = mavenSession.getProjectDependencyGraph();
-//        List<MavenProject> sortedProjects2 = projectDependencyGraph.getSortedProjects();
-//        if ( sortedProjects2 != null && !sortedProjects2.isEmpty() )
-//        {
-//            for ( MavenProject selectedProject : sortedProjects2 )
-//            {
-//                getLog().info( "sortedProjects2 projects:" + selectedProject );
-//            }
-//        }
-//
-////        RepositorySystemSession repositorySession = mavenSession.getRepositorySession();
-////        repositorySession.getLocalRepository().getBasedir();
-//
-//        //This is for things like mvn -pl a,b,c clean package 
-//        List<String> selectedProjects = mavenSession.getRequest().getSelectedProjects();
-//        if ( selectedProjects != null && !selectedProjects.isEmpty() )
-//        {
-//            for ( String selectedProject : selectedProjects )
-//            {
-//                getLog().info( "Selected projects:" + selectedProject );
-//            }
-//        }
-//
-//        List<MavenProject> sortedProjects = mavenSession.getProjects();
-//        if ( sortedProjects != null && !sortedProjects.isEmpty() )
-//        {
-//            for ( MavenProject mavenProject : sortedProjects )
-//            {
-//                getLog().info( "Project:" + mavenProject.getId() );
-//                List<ArtifactVersion> availableVersions = mavenProject.getArtifact().getAvailableVersions();
-//                for ( ArtifactVersion artifactVersion : availableVersions )
-//                {
-//
-//                    getLog().info( "  -> version: " + artifactVersion.toString() );
-//                }
-//            }
-//
-//            checkReactor( sortedProjects );
-//        }
-    }
+        VersionRangeRequest rangeRequest = new VersionRangeRequest();
+        rangeRequest.setArtifact( artifact );
+        rangeRequest.setRepositories( remoteRepositories );
 
-    private void checkReactor( List<MavenProject> sortedProjects )
-        throws MojoExecutionException
-    {
-        List<MavenProject> consistenceCheckResult = isReactorVersionConsistent( sortedProjects );
-        if ( !consistenceCheckResult.isEmpty() )
+        VersionRangeResult rangeResult;
+        try
         {
-            StringBuilder sb = new StringBuilder().append( SystemUtils.LINE_SEPARATOR );
-            for ( MavenProject mavenProject : consistenceCheckResult )
-            {
-                sb.append( " --> " );
-                sb.append( mavenProject.getId() );
-                sb.append( SystemUtils.LINE_SEPARATOR );
-            }
-            throw new MojoExecutionException( "The reactor contains different versions." + sb.toString() );
+            rangeResult = repoSystem.resolveVersionRange( repositorySystemSession, rangeRequest );
         }
-    }
-
-    private List<MavenProject> isReactorVersionConsistent( List<MavenProject> projectList )
-    {
-        List<MavenProject> result = new ArrayList<MavenProject>();
-
-        if ( projectList != null && !projectList.isEmpty() )
+        catch ( VersionRangeResolutionException e )
         {
-            String version = projectList.get( 0 ).getVersion();
-            getLog().debug( "First version:" + version );
-            for ( MavenProject mavenProject : projectList )
+            // TODO Auto-generated catch block
+            throw new MojoFailureException( "Failed", e );
+        }
+
+        List<Version> versions = rangeResult.getVersions();
+
+        if (versions !=null && !versions.isEmpty()) {
+            for ( Version version : versions )
             {
-                getLog().debug( " -> checking " + mavenProject.getId() );
-                if ( !version.equals( mavenProject.getVersion() ) )
-                {
-                    result.add( mavenProject );
-                }
+                getLog().info( " Item:" + version );
             }
         }
-        return result;
     }
 
 }
