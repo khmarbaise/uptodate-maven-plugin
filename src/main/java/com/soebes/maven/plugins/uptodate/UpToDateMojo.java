4e73328d153ff3056aaf64352e7064cdc9320779
Karl Heinz Marbaise <info@soebes.de>
2014-05-26 07:54:27 +0200

    - Safe state.

--- src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
+++ src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
@@ -10,14 +10,23 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
+import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.VersionRangeRequest;
 import org.eclipse.aether.resolution.VersionRangeResolutionException;
 import org.eclipse.aether.resolution.VersionRangeResult;
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transport.file.FileTransporterFactory;
+import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.eclipse.aether.version.Version;
 
 /**
@@ -54,10 +63,53 @@ public class UpToDateMojo
     @Parameter(defaultValue="${project.remotePluginRepositories}", readonly=true)
     private List<RemoteRepository> remotePluginRepositories;
     
+    private String localRepo;
+    
+    public static RepositorySystem newRepositorySystem()
+    {
+        /*
+         * Aether's components implement org.eclipse.aether.spi.locator.Service to ease manual wiring and using the
+         * prepopulated DefaultServiceLocator, we only need to register the repository connector and transporter
+         * factories.
+         */
+        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
+        locator.addService( RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class );
+        locator.addService( TransporterFactory.class, FileTransporterFactory.class );
+        locator.addService( TransporterFactory.class, HttpTransporterFactory.class );
+
+        locator.setErrorHandler( new DefaultServiceLocator.ErrorHandler()
+        {
+            @Override
+            public void serviceCreationFailed( Class<?> type, Class<?> impl, Throwable exception )
+            {
+                exception.printStackTrace();
+            }
+        } );
+
+        return locator.getService( RepositorySystem.class );
+    }
+
+    public static DefaultRepositorySystemSession newRepositorySystemSession( RepositorySystem system )
+    {
+        DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+
+        LocalRepository localRepo = new LocalRepository( "target/local-repo" );
+        session.setLocalRepositoryManager( system.newLocalRepositoryManager( session, localRepo ) );
+
+//        session.setTransferListener( new ConsoleTransferListener() );
+//        session.setRepositoryListener( new ConsoleRepositoryListener() );
+
+        // uncomment to generate dirty trees
+        // session.setDependencyGraphTransformer( null );
+
+        return session;
+    }
+
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
 
+	RepositorySystem newRepositorySystem = newRepositorySystem();
         Artifact artifact = new DefaultArtifact( "com.soebes.smpp:smpp:[0,)" );
 
         VersionRangeRequest rangeRequest = new VersionRangeRequest();
@@ -71,7 +123,6 @@ public class UpToDateMojo
         }
         catch ( VersionRangeResolutionException e )
         {
-            // TODO Auto-generated catch block
             throw new MojoFailureException( "Failed", e );
         }
 
@@ -82,6 +133,8 @@ public class UpToDateMojo
             {
                 getLog().info( " Item:" + version );
             }
+            Version highestVersion = rangeResult.getHighestVersion();
+            getLog().info(" Highest available version:" + highestVersion.toString());
         }
     }
 
