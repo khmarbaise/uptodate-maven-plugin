b9c8dcd4252c2c0c66f553522603c1ea8a7358ff
Karl Heinz Marbaise <info@soebes.de>
2014-06-20 12:08:18 +0200

    - Improved.

--- src/it/basicTest/verify.groovy
+++ src/it/basicTest/verify.groovy
@@ -23,23 +23,4 @@ println "Project version: ${projectVersion}"
 
 def buildLogFile = new File( basedir, "build.log");
 
-def distTargetNexus = new File (basedir, "dist/target/nexus");
-
-def files = [ 
-    "1/root-" + projectVersion + ".pom",
-    "2/module-two-" + projectVersion + ".pom",
-    "2/module-two-" + projectVersion + ".jar",
-    "3/module-one-" + projectVersion + ".pom",
-    "3/module-one-" + projectVersion + ".jar",
-    "4/dist-" +projectVersion + ".pom",
-]
-
-files.each {  
-   file -> check = new File(distTargetNexus, file)
-   println "checking ${check}"
-   if (!check.exists()) {
-       throw new FileNotFoundException( "Couldn't find " + file );
-   }
-}
-
 return true;
--- src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
+++ src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
@@ -1,9 +1,15 @@
 package com.soebes.maven.plugins.uptodate;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
+import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
+import org.apache.maven.artifact.resolver.ResolutionNode;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -12,57 +18,80 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.sonatype.aether.RepositorySystem;
-import org.sonatype.aether.RepositorySystemSession;
-import org.sonatype.aether.artifact.Artifact;
-import org.sonatype.aether.repository.RemoteRepository;
-import org.sonatype.aether.resolution.DependencyResolutionException;
-import org.sonatype.aether.util.artifact.DefaultArtifact;
-import org.sonatype.aether.util.artifact.JavaScopes;
-
-import com.jcabi.aether.Aether;
+import org.apache.maven.repository.RepositorySystem;
 
 /**
- * @author Karl-Heinz Marbaise <a
- *         href="mailto:khmarbaise@soebes.de">khmarbaise@soebes.de</a>
+ * @author Karl-Heinz Marbaise <a href="mailto:khmarbaise@soebes.de">khmarbaise@soebes.de</a>
  */
-@Mojo(name = "verify", defaultPhase = LifecyclePhase.VALIDATE, requiresProject = true, threadSafe = true)
-public class UpToDateMojo extends AbstractUpToDateMojo {
+@Mojo( name = "verify", defaultPhase = LifecyclePhase.VALIDATE, requiresProject = true, threadSafe = true )
+public class UpToDateMojo
+    extends AbstractUpToDateMojo
+{
 
     /**
      * The project currently being build.
      */
-    @Component
+    @Parameter( defaultValue = "${project}" )
     private MavenProject mavenProject;
 
     /**
      * The current Maven session.
      */
-    @Component
+    @Parameter( defaultValue = "${session}" )
     private MavenSession mavenSession;
 
     @Component
     private RepositorySystem repoSystem;
 
-    @Parameter(defaultValue = "${repositorySystemSession}")
-    private RepositorySystemSession repositorySystemSession;
+//    @Parameter( defaultValue = "${repositorySystemSession}" )
+//    private RepositorySystemSession repositorySystemSession;
+//
+//    @Parameter( defaultValue = "${project.remoteProjectRepositories}", readonly = true )
+//    private List<RemoteRepository> remoteRepositories;
+//
+//    @Parameter( defaultValue = "${project.remotePluginRepositories}", readonly = true )
+//    private List<RemoteRepository> remotePluginRepositories;
 
-    @Parameter(defaultValue = "${project.remoteProjectRepositories}", readonly = true)
-    private List<RemoteRepository> remoteRepositories;
+    private ArtifactResolutionResult resolve( String groupId, String artifactId, String version, String type) throws InvalidVersionSpecificationException
+    {
+//        Artifact artifact = new DefaultArtifact( groupId, artifactId, version, Artifact.SCOPE_COMPILE, type, null, null );
+        Artifact artifact = repoSystem.createProjectArtifact( groupId, artifactId, null );
+////        Artifact artifact = repoSystem.createArtifact( groupId, artifactId, null, type );
+        VersionRange newRange = VersionRange.createFromVersionSpec( version);
+////        artifact.setGroupId( groupId );
+////        artifact.setArtifactId( artifactId );
+        artifact.setVersionRange( newRange );
 
-    @Parameter(defaultValue = "${project.remotePluginRepositories}", readonly = true)
-    private List<RemoteRepository> remotePluginRepositories;
+        ArtifactResolutionRequest request = new ArtifactResolutionRequest();
+        request.setArtifact( artifact );
+        
+        request.setResolveRoot( true ).setResolveTransitively( false );
+        request.setServers( mavenSession.getRequest().getServers() );
+        request.setMirrors( mavenSession.getRequest().getMirrors() );
+        request.setProxies( mavenSession.getRequest().getProxies() );
+        request.setLocalRepository( mavenSession.getLocalRepository() );
+        request.setRemoteRepositories( mavenSession.getRequest().getRemoteRepositories() );
+        return repoSystem.resolve( request );
+    }
 
-    private String localRepo;
-
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        Artifact artifact = new DefaultArtifact("junit", "junit-dep", "", "jar", "4.10");
-        File repo = this.repositorySystemSession.getLocalRepository().getBasedir();
-        try {
-            Collection<Artifact> deps = new Aether(this.mavenProject, repo).resolve(artifact, JavaScopes.COMPILE);
-        } catch (DependencyResolutionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        ArtifactResolutionResult f;
+        try
+        {
+            f = resolve( "org.junit", "junit", "[3.8,)", "jar" );
+            Set<ResolutionNode> artifactResolutionNodes = f.getArtifactResolutionNodes();
+            getLog().info( "Solved: " + f );
+            getLog().info(" More: " + artifactResolutionNodes.isEmpty());
+            for ( ResolutionNode resolutionNode : artifactResolutionNodes )
+            {
+                getLog().info(" Node:" + resolutionNode.getArtifact().getId());
+            }
+        }
+        catch ( InvalidVersionSpecificationException e )
+        {
+            throw new MojoFailureException( "Version Range failed", e );
         }
     }
 
