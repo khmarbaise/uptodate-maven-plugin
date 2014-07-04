1c54e92b7f8e12d19092661f2d3e48872fb2df95
Karl Heinz Marbaise <info@soebes.de>
2014-07-04 18:09:11 +0200

    - Fixed.

--- src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
+++ src/main/java/com/soebes/maven/plugins/uptodate/UpToDateMojo.java
@@ -1,6 +1,5 @@
 package com.soebes.maven.plugins.uptodate;
 
-import java.io.File;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
@@ -10,6 +9,7 @@ import org.apache.maven.artifact.resolver.ArtifactResolutionException;
 import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
 import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
 import org.apache.maven.artifact.resolver.ResolutionNode;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
 import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.execution.MavenSession;
@@ -42,73 +42,67 @@ public class UpToDateMojo
     @Parameter( defaultValue = "${session}" )
     private MavenSession mavenSession;
 
-    
     @Component
     private ArtifactHandler artifactHandler;
-    
+
     @Component
     private RepositorySystem repository;
 
-    private ArtifactResolutionResult resolve( String groupId, String artifactId, String version, String type) throws InvalidVersionSpecificationException
-    {
-        VersionRange versionRange = VersionRange.createFromVersionSpec( "[" + version + ",)");
-//        VersionRange versionRange = VersionRange.createFromVersion( version);
+    
+    private void getNewestVersions() {
         
-        Artifact artifact = new DefaultArtifact( groupId, artifactId, versionRange, Artifact.SCOPE_COMPILE, type, null, artifactHandler );
-
-        ArtifactResolutionRequest request = new ArtifactResolutionRequest();
-        request.setArtifact( artifact );
-        request.setResolveRoot( true ).setResolveTransitively( false );
-        
-        request.setServers( mavenSession.getRequest().getServers() );
-        request.setMirrors( mavenSession.getRequest().getMirrors() );
-        request.setProxies( mavenSession.getRequest().getProxies() );
-        request.setLocalRepository( mavenSession.getLocalRepository() );
-        request.setRemoteRepositories( mavenSession.getRequest().getRemoteRepositories() );
-        return repository.resolve( request );
+        ArtifactVersions versions = getHelper().lookupArtifactVersions( artifact, false );
+        ArtifactVersion[] newer =
+                        versions.getNewerVersions( version, segment, Boolean.TRUE.equals( allowSnapshots ) );
     }
-    
-    
-    private File resolve(String artifactDescriptor) {
-        String[] s = artifactDescriptor.split(":");
 
-        String type = (s.length >= 4 ? s[3] : "jar");
-        Artifact artifact = repository.createArtifact(s[0], s[1], s[2], type);
+    private ArtifactResolutionResult resolve( String groupId, String artifactId, String version, String type )
+        throws InvalidVersionSpecificationException
+    {
+         VersionRange versionRange = VersionRange.createFromVersionSpec( "[" + version + ",)");
+//        VersionRange versionRange = VersionRange.createFromVersion( version );
+
+        Artifact artifact =
+            new DefaultArtifact( groupId, artifactId, versionRange, Artifact.SCOPE_COMPILE, type, null, artifactHandler );
 
         ArtifactResolutionRequest request = new ArtifactResolutionRequest();
-        request.setArtifact(artifact);
-       
-        request.setResolveRoot(true).setResolveTransitively(false);
-        request.setServers( mavenSession.getRequest().getServers() );
-        request.setMirrors( mavenSession.getRequest().getMirrors() );
-        request.setProxies( mavenSession.getRequest().getProxies() );
-        request.setLocalRepository(mavenSession.getLocalRepository());
-        request.setRemoteRepositories(mavenSession.getRequest().getRemoteRepositories());
-        repository.resolve(request);
-        return artifact.getFile();
+        request.setArtifact( artifact )
+            .setResolveRoot( true )
+            .setResolveTransitively( false )
+            .setServers( mavenSession.getRequest().getServers() )
+            .setMirrors( mavenSession.getRequest().getMirrors() )
+            .setProxies( mavenSession.getRequest().getProxies() )
+            .setLocalRepository( mavenSession.getLocalRepository() )
+            .setRemoteRepositories( mavenSession.getRequest().getRemoteRepositories() );
+
+        return repository.resolve( request );
     }
 
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
+
         ArtifactResolutionResult f;
         try
         {
-            f = resolve( "junit", "junit", "3.8", "jar" );
-            
-            for (ArtifactResolutionException exceptions : f.getErrorArtifactExceptions()) {
+             f = resolve( "junit", "junit", "3.8", "jar" );
+//            f = resolve( "org.hibernate", "hibernate", "3.5.1-Final", "pom" );
+
+            for ( ArtifactResolutionException exceptions : f.getErrorArtifactExceptions() )
+            {
                 getLog().error( "Failed:", exceptions );
             }
             Set<ResolutionNode> artifactResolutionNodes = f.getArtifactResolutionNodes();
             getLog().info( "Solved: " + f );
-            getLog().info(" More: " + artifactResolutionNodes.isEmpty());
+            getLog().info( " More: " + artifactResolutionNodes.isEmpty() );
             for ( ResolutionNode resolutionNode : artifactResolutionNodes )
             {
-                getLog().info(" Node:" + resolutionNode.getArtifact().getId());
+                getLog().info( " Node:" + resolutionNode.getArtifact().getId() );
             }
-            getLog().info( "artifacts Set:" + f.getArtifacts().isEmpty());
-            for (Artifact artifact : f.getArtifacts()) {
-                getLog().info("Artifact: " + artifact.getId());
+            getLog().info( "artifacts Set:" + f.getArtifacts().isEmpty() );
+            for ( Artifact artifact : f.getArtifacts() )
+            {
+                getLog().info( "Artifact: " + artifact.getId() );
             }
         }
         catch ( InvalidVersionSpecificationException e )
