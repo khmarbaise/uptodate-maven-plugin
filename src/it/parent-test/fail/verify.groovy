1950f9816ab98dfa92bd55f32533ad05b0230e30
Karl-Heinz Marbaise <info@soebes.de>
2014-08-17 13:05:53 +0200

    - Fixed #3   Fixed integration tests cause during the release process   the version of the plugin changed.

--- src/it/parent-test/fail/verify.groovy
+++ src/it/parent-test/fail/verify.groovy
@@ -1,4 +1,22 @@
+
+def getProjectVersion() {
+   def pom = new XmlSlurper().parse(new File(basedir, 'pom.xml'))
+
+   def allPlugins = pom.build.plugins
+
+   def dependencies = allPlugins.plugin
+   def uptodatePlugin = dependencies.find {
+       item -> item.groupId.equals("com.soebes.maven.plugins") && item.artifactId.equals("uptodate-maven-plugin");
+   }
+
+   return uptodatePlugin.version;
+}
+
+def projectVersion = getProjectVersion();
+
 def buildLogFile = new File( basedir, "build.log");
 
-assert buildLogFile.text.contains ('[ERROR] Failed to execute goal com.soebes.maven.plugins:uptodate-maven-plugin:0.1.0-SNAPSHOT:parent (parent-check) on project root: There is a more up-to-date version ( 0.2 ) of the parent available. -> [Help 1]')
+println "Version:" + projectVersion
+
+assert buildLogFile.text.contains ('[ERROR] Failed to execute goal com.soebes.maven.plugins:uptodate-maven-plugin:' + projectVersion + ':parent (parent-check) on project root: There is a more up-to-date version ( 0.2 ) of the parent available. -> [Help 1]')
 
